e09eda17c13606a502ef1445e717fb75b6055d43
Ammar Shahzad <sp24-bse-012@cuilahore.edu.pk>
2026-04-25 03:48:20 +0500

    Add implementation of math functions in C++

---
+++ c2/math.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <String>   //all the method related to C++
+#include <cmath>    // all the material related to math
+
+using namespace std;
+int main(){
+	cout <<max(45, 4875)<< "\n";
+	cout << min(45,551)<< "\n";
+	cout << sqrt(144)<< "\n";
+	cout << round(45.578)<< "\n";
+	cout << log(sin(90))<< "\n";
+	return 0;
+}
