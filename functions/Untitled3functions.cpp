4d1c4c5fc2baea4edf57904185f34de27a116087
Ammar Shahzad <sp24-bse-012@cuilahore.edu.pk>
2026-04-25 04:04:25 +0500

    func

---
+++ functions/Untitled3functions.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<String>
+using namespace std;
+void str(int a){ // creating methods
+	if(a==1){
+		cout<< "ok good";
+		
+	}
+}
+int main(){
+	int a = 1;
+	str(a);
+	return 0;
+}
