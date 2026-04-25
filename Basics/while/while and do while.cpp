f57b95ce0714b884ac7aeaeef82be1cf4169bd9c
Ammar Shahzad <sp24-bse-012@cuilahore.edu.pk>
2026-04-25 19:52:02 +0500

    while

---
+++ Basics/while/while and do while.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<String>
+using namespace std;
+int main(){
+	int i = 10;
+	while(i<=20){
+		cout<< i<<" ";
+		i++;
+	}
+	
+	cout << "\n";
+	
+	int a = 5;
+	do{
+	cout << a << " ";
+	a = a+5;	
+	}while(a<50);
+	
+	return 0;
+}
+
