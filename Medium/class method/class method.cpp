abe4435fe82426429321b603b71726a2496af81d
Ammar Shahzad <sp24-bse-012@cuilahore.edu.pk>
2026-04-25 19:56:59 +0500

    class method

---
+++ Medium/class method/class method.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<String>
+using namespace std;
+class mc{ // adding methods to class
+	public:
+		void method(){
+			cout << "hi there";
+		}
+};
+int main (){
+	mc ab;
+	ab.method();
+	return 0;
+}
